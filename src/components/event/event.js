e37ddc7060c15ec2c6a1d79cf04ef6b76218b731
mehul <127575583+Me-cha@users.noreply.github.com>
2023-09-08 10:25:46 +0530

    event input

--- src/components/event/event.js
+++ src/components/event/event.js
@@ -1,4 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { storage, db } from "../../config/firebase";
+import {
+  setDoc,
+  collection,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  doc,
+  Timestamp,
+} from "firebase/firestore";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  listAll,
+  list,
+} from "firebase/storage";
+import { v4 } from "uuid";
 import Header from "../header/header";
 import "./event.css";
 
@@ -9,18 +27,54 @@ function EventInput() {
   const [plannerLastName, setPlannerLastName] = useState("");
   const [venue, setVenue] = useState("");
   const [startDate, setStartDate] = useState("");
-  const [startTime, setStartTime] = useState("");
   const [endDate, setEndDate] = useState("");
-  const [endTime, setEndTime] = useState("");
   const [timeZone, setTimeZone] = useState("");
   const [notification, setNotification] = useState(false);
+  const [file, setFile] = useState("");
+  const [imageUrls, setImageUrls] = useState([]);
 
   const handleAddSchedule = () => {
     // Logic to add schedule
   };
 
-  const handleCreateEvent = () => {
-    // Logic to create event
+
+  useEffect(() => {
+    if (imageUrls.length > 0) {
+      addEventToDatabase();
+    }
+  }, [imageUrls]);
+
+  const addEventToDatabase = async () => {
+    try {
+
+      const EventStart = new Date(startDate);
+      const EventEnd = new Date(endDate);
+
+      await setDoc(doc(db, "event", eventTitle), {
+        Event_name: eventTitle,
+        Event_category: eventCategory,
+        Event_organizor: plannerFirstName+" "+plannerLastName,
+        Event_venue: venue,
+        Event_start: EventStart,
+        Event_end: EventEnd,
+        // Event_start_Time: startTime,
+        // Event_end_Time: endTime,
+        Event_notification: notification,
+        Event_IMAGE: imageUrls[0],
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleUpload = () => {
+    if (file == null) return null;
+    const imageRef = ref(storage, `event/${file.name}.${v4()}`);
+    uploadBytes(imageRef, file).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls([url]);
+      });
+    });
   };
 
   return (
@@ -30,50 +84,51 @@ function EventInput() {
       </div>
       <form
         style={{
-          padding: "3vh 10vw",
-          margin: "10vh auto 2vh auto",
+          padding: "3vh 4vw",
+          margin: "14vh auto 2vh auto",
           border: "2px solid white",
-          height: "90vh",
+          height: "105vh",
           width: "60vw",
-          backgroundColor:"white",
+          backgroundColor: "white",
         }}
       >
         <div style={{ fontSize: "20px", fontWeight: "bolder" }}>
           Basic Information
         </div>
-        <div>
-          <div>
-            {" "}
+
+        <div className="EventMandatoryField">
+          <div className="event_title">
             <label htmlFor="eventTitle">Event Title</label>
+            <input
+              placeholder=" Event Title"
+              type="text"
+              style={{
+                width: "52vw",
+              }}
+              value={eventTitle}
+              onChange={(e) => setEventTitle(e.target.value)}
+            />
           </div>
-          <input
-            placeholder=" Event Title"
-            type="text"
-            id="eventTitle"
-            value={eventTitle}
-            onChange={(e) => setEventTitle(e.target.value)}
-          />
-        </div>
 
-        <div>
-          <div>
+          <div className="event_category">
             <label htmlFor="eventCategory">Event Category</label>
+            <br />
+            <input
+              placeholder=" Event Category"
+              type="text"
+              id="eventCategory"
+              value={eventCategory}
+              onChange={(e) => setEventCategory(e.target.value)}
+            />
           </div>
-          <input
-            placeholder=" Event Category"
-            type="text"
-            id="eventCategory"
-            value={eventCategory}
-            onChange={(e) => setEventCategory(e.target.value)}
-          />
-        </div>
 
-        <div>
-          <label>Planner</label>
-          <div>
+          <div className="event_plannner">
+            <label>Planner</label>
+            <br />
             <input
               placeholder=" First Name"
               type="text"
+              style={{ marginRight: "3vw" }}
               value={plannerFirstName}
               onChange={(e) => setPlannerFirstName(e.target.value)}
             />
@@ -84,97 +139,125 @@ function EventInput() {
               onChange={(e) => setPlannerLastName(e.target.value)}
             />
           </div>
+          <div className="location_Lable" style={{fontSize:"15pt",fontWeight:"600"}}>
+            LOCATION
+          </div>
+          <div
+            className="img_dis"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              height: "10vh",
+              width: "10vw",
+              border: "none",
+              alignItems: "center",
+              top: "-23vh",
+              left: "46vw",
+            }}
+          >
+            {file && (
+              <a href={URL.createObjectURL(file)} target="_blank">
+                <img
+                  src={URL.createObjectURL(file)}
+                  alt="user-uploaded media"
+                  style={{
+                    position: "relative",
+                    right: "11vw",
+                    border: "1px solid #7B43AC",
+                    borderRadius: "10px",
+                    objectFit: "cover",
+                  }}
+                />
+              </a>
+            )}
+            <input
+              type="file"
+              className="eventIMG"
+              style={{
+                position: "absolute",
+                margin: "10vh 0vw 0vh 10vw",
+              }}
+              onChange={(e) => setFile(e.target.files[0])}
+              accept="image/*, video/*"
+            />
+          </div>
         </div>
 
-        <div>
+        <div className="EventDescription">
           <div>
-            {" "}
             <label htmlFor="venue">Venue</label>
+            <br />
+            <input
+              type="text"
+              placeholder="  venue"
+              value={venue}
+              style={{ width: "52vw" }}
+              onChange={(e) => setVenue(e.target.value)}
+            />
           </div>
+
+          <div style={{fontSize:"15pt",fontWeight:"600"}}>EVENT DATES</div>
+          <div
+            className="EventDates"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <div>
+              <label>Event Start</label>
+              <br />
+              <input
+                placeholder=" Event Start"
+                type="datetime-local"
+                value={startDate}
+                onChange={(e) => setStartDate(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label>Event End</label>
+              <br />
+              <input
+                placeholder=" Event End"
+                type="datetime-local"
+                value={endDate}
+                onChange={(e) => setEndDate(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <label>Time Zone</label>
+            </div>
+            <input
+              placeholder=" Time Zone"
+              type="text"
+              id="timeZone"
+              value={timeZone}
+              onChange={(e) => setTimeZone(e.target.value)}
+            />
+          </div>
+        </div>
+        <div>
+          <div>
+            <div style={{fontSize:"15pt",fontWeight:"600"}}>SCHEDULES</div>
+          </div>
+          <div>
+            <button style={{marginLeft:"10vw",}} onClick={handleAddSchedule}>Add Schedule</button>
+          </div>
+        </div>
+
+        <div style={{ margin:"auto 20vw" }}>
           <input
-            type="text"
-            id="venue"
-            value={venue}
-            onChange={(e) => setVenue(e.target.value)}
+            type="checkbox"
+            checked={notification}
+            style={{ height: "auto",margin:"auto 0.5vw" }}
+            onChange={(e) => setNotification(e.target.checked)}
           />
-        </div>
-
-        <div>
-          <label>EVENT DATES</label>
-          <div>
-            <div>
-              <label>Start Date</label>
-            </div>
-            <input
-              placeholder=" Start Date"
-              type="date"
-              value={startDate}
-              onChange={(e) => setStartDate(e.target.value)}
-            />
-
-            <div>
-              <label>Start Time</label>
-            </div>
-            <input
-              placeholder=" Start Time"
-              type="time"
-              value={startTime}
-              onChange={(e) => setStartTime(e.target.value)}
-            />
-
-            <div>
-              <label>End Date</label>
-            </div>
-            <input
-              placeholder=" End Date"
-              type="date"
-              value={endDate}
-              onChange={(e) => setEndDate(e.target.value)}
-            />
-
-            <div>
-              <label>End Time</label>
-            </div>
-            <input
-              placeholder="End Time"
-              type="time"
-              value={endTime}
-              onChange={(e) => setEndTime(e.target.value)}
-            />
-          </div>
-        </div>
-
-        <div>
-          <div>
-            <label>Time Zone</label>
-          </div>
-          <input
-            placeholder=" Time Zone"
-            type="text"
-            id="timeZone"
-            value={timeZone}
-            onChange={(e) => setTimeZone(e.target.value)}
-          />
-        </div>
-
-        <div>
-          <div>
-            <label>SCHEDULES</label>
-          </div>
-          <div>
-            <button onClick={handleAddSchedule}>Add Schedule</button>
-          </div>
-        </div>
-
-        <div style={{margin:"auto 15vw"}}>
-          <label>
-            <input
-              type="checkbox"
-              checked={notification}
-              onChange={(e) => setNotification(e.target.checked)}
-            />
-            Notifications
-          </label>
+          <label style={{color:"black"}}>Notifications</label>
         </div>
       </form>
       <div>
@@ -191,7 +274,7 @@ function EventInput() {
             borderRadius: "10px",
             color: "#FFFFFF",
           }}
-          onClick={handleCreateEvent}
+          onClick={handleUpload}
         >
           Create Event
         </button>
@@ -201,28 +284,6 @@ function EventInput() {
 }
 
 export default EventInput;
-
-// import "./event.css";
-// import React, { useState, useEffect } from "react";
-// import { storage, db } from "../../config/firebase";
-
-// import {
-//   setDoc,
-//   collection,
-//   addDoc,
-//   deleteDoc,
-//   updateDoc,
-//   doc,
-// } from "firebase/firestore";
-// import {
-//   ref,
-//   uploadBytes,
-//   getDownloadURL,
-//   listAll,
-//   list,
-// } from "firebase/storage";
-// import { v4 } from "uuid";
-// import Header from "../header/header";
 
 // const EventInput = () => {
 //   const [eventName, setEventName] = useState("");
@@ -238,45 +299,6 @@ export default EventInput;
 
 //   const onSubmit = () => {
 //     handleUpload();
-//   };
-
-//   // const Ref = collection(db, "event",eventName);
-//   // await setDoc(Ref,`{eventname}`);
-
-//   useEffect(() => {
-//     if (imageUrls.length > 0) {
-//       addEventToDatabase();
-//     }
-//   }, [imageUrls]);
-
-//   const addEventToDatabase = async () => {
-//     try {
-//       const EventStart = new Date(event_start);
-//       const EventEnd = new Date(event_end);
-//       await setDoc(doc(db, "event",eventName), {
-//         Event_name: eventName,
-//         Event_brief: eventBrief,
-//         Event_organizor: eventorganizor,
-//         Event_address: eventvenue,
-//         Event_Contact: organizor_contact,
-//         Event_start: EventStart,
-//         Event_end: EventEnd,
-//         Event_notification: eventnotification,
-//         Event_IMAGE: imageUrls[0],
-//       });
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-
-//   const handleUpload = () => {
-//     if (file == null) return null;
-//     const imageRef = ref(storage, `event/${file.name}.${v4()}`);
-//     uploadBytes(imageRef, file).then((snapshot) => {
-//       getDownloadURL(snapshot.ref).then((url) => {
-//         setImageUrls([url]);
-//       });
-//     });
 //   };
 
 //   return (
